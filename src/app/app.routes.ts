ebf681501253dfcac9f736a9ffd8896d1d41adc0
KungFuKelo <116620255+KungFuKelo@users.noreply.github.com>
2024-02-14 21:28:37 +0200

    nav-side.component (Done)
    - nav-side
    - router created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { OrdersComponent } from './pages/ordersPages/orders/orders.component';
+import { OneOrderComponent } from './pages/ordersPages/one-order/one-order.component';
+import { HomeComponent } from './pages/homePages/home.component';
+import { ProductsComponent } from './pages/productsPages/products/products.component';
+import { OneProductComponent } from './pages/productsPages/one-product/one-product.component';
+import { UsersComponent } from './pages/usersPages/users/users.component';
+import { OneUserComponent } from './pages/usersPages/one-user/one-user.component';
+import { AddProductComponent } from './pages/productsPages/add-product/add-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'orders',
+    children: [
+      { path: '', component: OrdersComponent },
+      { path: ':order_ID', component: OneOrderComponent },
+    ],
+  },
+  {
+    path: 'products',
+    children: [
+      { path: '', component: ProductsComponent },
+      { path: ':product_ID', component: OneProductComponent },
+      { path: 'add', component: AddProductComponent },
+    ],
+  },
+  {
+    path: 'users',
+    children: [
+      { path: '', component: UsersComponent },
+      { path: ':user_ID', component: OneUserComponent },
+    ],
+  },
+];
--- src/app/components/navComponents/nav-side/nav-side.component.ts
+++ src/app/components/navComponents/nav-side/nav-side.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-side',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgClass],
   templateUrl: './nav-side.component.html',
-  styleUrl: './nav-side.component.css'
+  styleUrl: './nav-side.component.css',
 })
 export class NavSideComponent {
+  @Input() user!: {
+    message: string;
+    token: string;
+    username: string;
+    authorities: {
+      users: boolean;
+      orders: boolean;
+      products: boolean;
+      categories: boolean;
+    };
+  };
 
+  selectedClass = 'bg-dark text-light';
+  selectedTab = 'home';
+
+  select(name: string) {
+    this.selectedTab = name;
+  }
 }
