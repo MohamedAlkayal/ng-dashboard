ddb8e4b83997c43d25c7ccea439bd738d7e71333
KungFuKelo <116620255+KungFuKelo@users.noreply.github.com>
2024-02-14 18:29:13 +0200

    updating styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavSideComponent } from './components/navComponents/nav-side/nav-side.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavSideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dashboard';
+  user = {
+    message: 'Login successful',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzkyZWYwYjZhYjE3YzdmY2IwMzQ5MiIsImF1dGhvcml0aWVzIjp7IlVzZXJzIjp0cnVlLCJPcmRlcnMiOnRydWUsIlByb2R1Y3RzIjp0cnVlLCJDYXRlZ29yaWVzIjp0cnVlfSwiaWF0IjoxNzA3OTIyNTI2LCJleHAiOjE3MDc5Mjk3MjZ9.HnIKeakfrmTT-2sgCs6zfPDIldVNStCCmm8Za95KTgA',
+    authorities: {
+      Users: true,
+      Orders: true,
+      Products: true,
+      Categories: true,
+    },
+  };
 }
--- src/app/components/navComponents/nav-side/nav-side.component.ts
+++ src/app/components/navComponents/nav-side/nav-side.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-side',
   standalone: true,
   imports: [],
   templateUrl: './nav-side.component.html',
-  styleUrl: './nav-side.component.css'
+  styleUrl: './nav-side.component.css',
 })
 export class NavSideComponent {
-
+  @Input() authorities!: {
+    Users: boolean;
+    Orders: boolean;
+    Products: boolean;
+    Categories: boolean;
+  };
 }
