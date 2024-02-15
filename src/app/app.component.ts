2b5ccdabac606780373708d337b77a63ead54f5c
mahmoud.morsi <mahmoudmorsi330@gmail.com>
2024-02-15 22:03:04 +0200

    Filters, dropdown and Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { FilterTextComponent } from './components/filtersComponents/filter-text/filter-text.component';
+import { FilterNumberComponent } from './components/filtersComponents/filter-number/filter-number.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgControl } from '@angular/forms';
+import { FilterDropdownComponent } from './components/filtersComponents/filter-dropdown/filter-dropdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,FilterDropdownComponent,FilterTextComponent,FilterNumberComponent,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+  ngOnInit(): void {
+
+  }
+  city:string[]=["Cairo","Giza","Zagazig","Alex"]
   title = 'dashboard';
+ @Input() numFilterValue(data:any){
+    console.log(data)
+  }
+
 }
