6e4a4650256c00d212afd60f3a4bafad72e3bbfd
KungFuKelo <116620255+KungFuKelo@users.noreply.github.com>
2024-02-21 18:02:12 +0200

    alo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { InputTextareaComponent } from './components/formComponents/input-textarea/input-textarea.component';
 import { InputInnerLableComponent } from './components/formComponents/input-inner-lable/input-inner-lable.component';
 import { NavSideComponent } from './components/navComponents/nav-side/nav-side.component';
-import { AdminServices } from './services/admin/admin.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavSideComponent, HttpClientModule],
-  providers: [AdminServices],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dashboard';
 
-  constructor(private auth: AdminServices) {}
+  // constructor(private auth: AdminServices) {}
 
   // ngOnInit() {
   //   this.auth.loginAdmin("")
