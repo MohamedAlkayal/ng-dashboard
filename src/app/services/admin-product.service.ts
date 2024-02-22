a5aeb484210075575706c5b6d92419e8e97f77ae
m-ehab2 <muhamedehab1@gmail.com>
2024-02-23 01:58:42 +0200

    rearranged the placement of some variables

--- src/app/services/admin-product.service.ts
+++ src/app/services/admin-product.service.ts
@@ -29,15 +29,15 @@ export class AdminProductService {
   getAllProducts(
     page: number = 1,
     limit: number = 10,
+    sortBy?: string,
+    order?: string,
     id_modelNumber?: string,
     name?: string,
     brandName?: string,
     category?: string,
     subCategory?: string,
     val1?: number,
-    val2?: number,
-    sortBy?: string,
-    order?: string
+    val2?: number
   ) {
     const headers = this.token.getHeader();
     let url = `${this.productsUrl}?page=${page}&limit=${limit}`;
@@ -65,7 +65,6 @@ export class AdminProductService {
     if (order) {
       url += `&order=${order}`;
     }
-    console.log(url);
     return this.http.get(url, { headers: headers });
   }
   getOneProduct(id: string) {
