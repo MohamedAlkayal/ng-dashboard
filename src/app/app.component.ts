8c4defa612b10b2cf187cd4c11cdd2961b1b71da
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-03-01 04:22:38 +0200

    toasts and roasts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TokenUtilsService } from './services/token/token-utils.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,16 @@ import { TokenUtilsService } from './services/token/token-utils.service';
 })
 export class AppComponent {
   title = 'dashboard';
-  constructor(private tokenService: TokenUtilsService) {}
+  constructor(private tokenService: TokenUtilsService,private toaster:ToastrService) {}
 
   user!: any;
 
+  // showData(){
+  //   this.toaster.success('Welcome to my app!',"success");
+  //   this.toaster.error('out from my app!',"error");
+  //   this.toaster.info('Welcome to my app!',"info");
+  //   this.toaster.warning('Welcome to my app!',"warning");
+  // }
   ngOnInit() {
     setTimeout(() => {
       this.tokenService.cheackForTokenExpiration();
--- src/app/pages/homePages/home.component.ts
+++ src/app/pages/homePages/home.component.ts
@@ -1,48 +1,74 @@
 import { Component } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { TableComponent } from '../../components/tableComponents/table/table.component';
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ChartModule,TableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-//   data: any;
-    
-//   options: any;
-  
-  constructor() {
-//       this.data = {
-//           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//           datasets: [
-//               {
-//                   label: 'First Dataset',
-//                   data: [65, 59, 80, 81, 56, 55, 40]
-//               },
-//               {
-//                   label: 'Second Dataset',
-//                   data: [28, 48, 40, 19, 86, 27, 90]
-//               }
-//           ]
-//       }
-      
-//       this.options = {
-//           title: {
-//               display: true,
-//               text: 'My Title',
-//               fontSize: 16
-//           },
-//           legend: {
-//               position: 'bottom'
-//           }
-//       };
-//   }
-//   doSomething(){
-//     console.log("hello");
-    
+    constructor() {
+      this.dataBar = {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {  
+                 label: 'First Dataset',
+                  data: [65, 59, 80, 81, 56, 55, 40]
+              }
+          ]
+      }
+      this.dataPie = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {  
+               label: 'First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }
+        ]
+    }
+    this.dataLine = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+          {  
+             label: 'First Dataset',
+              data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {  
+            label: 'Second Dataset',
+             data: [50, 40, 60, 70, 80,60, 56]
+         }
+      ]
   }
+      
+      this.options = {
+          title: {
+              display: true,
+              text: 'My Title',
+              fontSize: 16
+          },
+          legend: {
+              position: 'bottom'
+          }
+      };
+  }
+    
+  dataBar: any;
+  dataPie: any;
+  dataLine: any;
+  options: any;
+  topUsers:any[]=[]
+
+
+  tableCols = [
+    { lable: 'Top Users', colData: 'Top Users' },
+  ];
+
+
+
 }
 
